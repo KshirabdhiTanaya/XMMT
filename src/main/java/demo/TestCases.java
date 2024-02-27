d9975aa4131a8ab5ca15ea49aeb22cc9f2748e41
khirabdhitanya <khirabdhitanya@gmail.com>
2024-02-27 14:02:43 +0530

    ME_QA_XGOOGLE_CALC1_MODULE_L1

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -1,12 +1,17 @@
 package demo;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.util.logging.Level;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -33,7 +38,7 @@ public class TestCases {
 
         // Set browser to maximize and wait
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 
     }
 
@@ -46,11 +51,127 @@ public class TestCases {
     }
 
     
-    public  void testCase01(){
+    // public  void testCase01(){
+    //     System.out.println("Start Test case: testCase01");
+    //     driver.get("https://www.google.com");
+    //     System.out.println("end Test case: testCase02");
+    // }
+    public void testCase01() {
         System.out.println("Start Test case: testCase01");
-        driver.get("https://www.google.com");
-        System.out.println("end Test case: testCase02");
+        driver.get("https://www.makemytrip.com/");
+        String currentUrl = driver.getCurrentUrl();
+        if (currentUrl.contains("makemytrip")) {
+            System.out.println("URL verification passed: " + currentUrl);
+        } else {
+            System.out.println("URL verification failed: " + currentUrl);
+        }
+        System.out.println("End Test case: testCase01");
     }
+    public void testCase02() throws InterruptedException {
+        System.out.println("Start Test case: testCase02");
+        driver.get("https://www.makemytrip.com/ ");
+        Thread.sleep(3000);
 
+        driver.findElement(By.id("fromCity")).click();
+        WebElement departureLocation = driver.findElement(By.xpath("//input[@placeholder='From']"));
+        departureLocation.click();
+        departureLocation.sendKeys("blr");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[@class='calc60']")).click();
+        Thread.sleep(2000);
 
+        WebElement arrivalLocation = driver.findElement(By.id("toCity"));
+        arrivalLocation.sendKeys("DEL");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[@class='calc60']")).click();
+        Thread.sleep(2000);
+
+        WebElement depatureDate = driver.findElement(By.xpath("(//p[text()='29'])[2]/parent::div"));
+        depatureDate.click();
+        WebElement searchButton = driver.findElement(By.xpath("//a[contains(@class,'primaryBtn')]"));
+        searchButton.click();
+
+        WebElement flightDetails = driver.findElement(By.xpath("(//div[@class='makeFlex'])[2]"));
+        String flightDetailsData = flightDetails.getText();
+        System.out.print("Flight details : " + flightDetailsData + " ");
+
+        WebElement flightPrice = driver.findElement(By.xpath("//div[contains(@class,'clusterViewPrice')]"));
+        String flightPriceAdult = flightPrice.getText();
+        System.out.println("Flight Price:" + flightPriceAdult);
+
+    }
+    public void testCase03() throws InterruptedException {
+        System.out.println("Start Test case: testCase03");
+        driver.get("https://www.makemytrip.com/ ");
+        Thread.sleep(3000);
+        driver.findElement(By.className("menu_Trains")).click();
+
+        driver.findElement(By.id("fromCity")).click();
+        WebElement departureLocation = driver.findElement(By.xpath("//input[@placeholder='From']"));
+        departureLocation.click();
+        departureLocation.sendKeys("ypr");
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//div[@class='makeFlex']")).click();
+        Thread.sleep(2000);
+
+        WebElement arrivalLocation = driver.findElement(By.xpath("//input[@placeholder='To']"));
+        arrivalLocation.sendKeys("ndls");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[@class='makeFlex']")).click();
+        Thread.sleep(2000);
+
+        WebElement depatureDate = driver.findElement(By.xpath("//div[@aria-label='Fri Mar 29 2024']"));
+        depatureDate.click();
+
+        driver.findElement(By.xpath("//li[@data-cy='3A']")).click();
+
+        WebElement searchButton = driver.findElement(By.xpath("//a[contains(@class,'primaryBtn')]"));
+        searchButton.click();
+
+        WebElement trainDetails = driver.findElement(By.xpath("//div[@class='flex train-info']"));
+        String trainDetailsData = trainDetails.getText();
+        System.out.println("Train details : " + trainDetailsData + " ");
+
+        WebElement trainPrice = driver.findElement(By.xpath("//div[contains(@class,'ticket-price')]"));
+        String price = trainPrice.getText();
+        System.out.println("3AC train Price:" + price);
+
+    }
+    public void testCase04() throws InterruptedException{
+        System.out.println("Start Test case: testCase04");
+        driver.get("https://www.makemytrip.com/ ");
+        Thread.sleep(3000);
+        driver.findElement(By.className("menu_Buses")).click();
+
+        driver.findElement(By.id("fromCity")).click();
+        WebElement departureLocation = driver.findElement(By.xpath("//input[@placeholder='From']"));
+        departureLocation.click();
+        departureLocation.sendKeys("bangl");
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//div[@class='makeFlex column']")).click();
+        Thread.sleep(2000);
+
+        WebElement arrivalLocation = driver.findElement(By.xpath("//input[@placeholder='To']"));
+        arrivalLocation.sendKeys("del");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[@class='makeFlex column']")).click();
+        Thread.sleep(2000);
+
+        WebElement depatureDate = driver.findElement(By.xpath("//div[@aria-label='Fri Mar 29 2024']"));
+        depatureDate.click();
+
+        WebElement searchButton = driver.findElement(By.id("search_button"));
+        searchButton.click();
+         
+        WebElement error = driver.findElement(By.xpath("//span[@class='error-title']"));
+        String errorMessage = error.getText();
+        if(errorMessage.equals("No buses found for 29 Mar")){
+            System.out.println("TestCase pass");
+
+        }else{
+            System.out.println("TestCase Fail");
+        }
+    }
 }
+  
+
